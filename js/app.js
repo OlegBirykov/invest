b7165717a68faaac0ae3229c0dc97cd851dfe830
Di <di@cherryline.ru>
2022-06-10 14:06:20 +0300

    Добавлена форма авторизации

--- js/app.js
+++ js/app.js
@@ -5,28 +5,9 @@ const sidePanel = $('.header__side-panel');
 const closeButton = $('.header__side-close-button');
 const sideMenuLink = $('.header__side-menu .header__menu-link');
 
-$(document).ready(function() {
-    popupHeader.html(staticHeader.html());
-    popupHeader.toggleClass('header__content_popup_show', $(window).scrollTop() >= staticHeader.height());
-
-    $('.header__hamburger-link').click(function(e) {
-        e.preventDefault();
-        $('body').addClass('header__app-noscrolling');
-
-        overlay.addClass('header__overlay_show');
-        overlay.animate({ opacity: "0.6" }, 500);
-
-        sidePanel.css({ right: `-${sidePanel.width()}px` });
-        sidePanel.addClass('header__side-panel_show');
-        sidePanel.animate({ right: "0" }, 500);
-    });
-
-    setTimeout(() => $('.header__logo').css({ opacity: "1" }), 1000);
-});
-
-$(window).scroll(function() {
-    popupHeader.toggleClass('header__content_popup_show', $(window).scrollTop() >= staticHeader.height());
-});
+const loginPage = $('.popup#login');
+const loginOverlay = $('.popup#login .popup__overlay');
+const loginForm = $('.popup#login .popup__window');
 
 function closeMenu(href) {
     const overlayOff = () => {
@@ -45,6 +26,59 @@ function closeMenu(href) {
     sidePanel.animate({ right: `-${sidePanel.width()}px` }, 500, 'swing', menuOff);
 }
 
+function openLoginForm() {
+    loginPage.addClass('popup_show');
+    loginOverlay.addClass('popup__overlay_show');
+    loginForm.addClass('popup__window_show');
+
+    loginOverlay.animate({ opacity: "0.6" }, 250, '', () => {
+        loginForm.animate({ opacity: "1" }, 250);
+    });
+}
+
+function closeLoginForm() {
+    loginForm.animate({ opacity: "0" }, 250, '', () => {
+        loginOverlay.animate({ opacity: "0" }, 250, '', () => {
+            loginPage.removeClass('popup_show');
+            loginOverlay.removeClass('popup__overlay_show');
+            loginForm.removeClass('popup__window_show');
+        });
+    });
+}
+
+$(document).ready(function() {
+    popupHeader.html(staticHeader.html());
+    popupHeader.toggleClass('header__content_popup_show', $(window).scrollTop() >= staticHeader.height());
+
+    $('.header__hamburger-link').click(function(e) {
+        e.preventDefault();
+        $('body').addClass('header__app-noscrolling');
+
+        overlay.addClass('header__overlay_show');
+        overlay.animate({ opacity: "0.6" }, 500);
+
+        sidePanel.css({ right: `-${sidePanel.width()}px` });
+        sidePanel.addClass('header__side-panel_show');
+        sidePanel.animate({ right: "0" }, 500);
+    });
+
+    $('.header__button').click(function(e) {
+        e.preventDefault();
+        openLoginForm();
+    });
+
+    $('.header__user').click(function(e) {
+        e.preventDefault();
+        openLoginForm();
+    });
+
+    setTimeout(() => $('.header__logo').css({ opacity: "1" }), 1000);
+});
+
+$(window).scroll(function() {
+    popupHeader.toggleClass('header__content_popup_show', $(window).scrollTop() >= staticHeader.height());
+});
+
 overlay.click(() => closeMenu());
 
 closeButton.click(() => closeMenu());
@@ -53,4 +87,9 @@ sideMenuLink.click(function(e) {
     e.preventDefault();
     closeMenu(e.target.href);
 });
+
+loginOverlay.click(() => closeLoginForm());
+
+
+
 
